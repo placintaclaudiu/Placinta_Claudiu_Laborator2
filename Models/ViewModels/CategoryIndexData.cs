24d607f6b51883d418bcc8e055f1562e3d053a01
Placinta Claudiu <claudiuplacinta33@gmail.com>
2022-11-08 21:01:07 +0200

    Completare de la lab 4

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Placinta_Claudiu_Laborator2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Placinta_Claudiu_Laborator2.Data;
 using Placinta_Claudiu_Laborator2.Models;
+using Placinta_Claudiu_Laborator2.Models.ViewModels;
 
 namespace Placinta_Claudiu_Laborator2.Pages.Categories
 {
@@ -20,13 +21,34 @@ namespace Placinta_Claudiu_Laborator2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                    .ThenInclude(i => i.Book)
+                    .ThenInclude(i => i.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
+            }
+
+            /*
             if (_context.Category != null)
             {
                 Category = await _context.Category.ToListAsync();
             }
+            */
         }
     }
 }
