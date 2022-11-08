57394421112268402a86839d7350277324f2f90f
Placinta Claudiu <claudiuplacinta33@gmail.com>
2022-11-08 20:49:29 +0200

    Sarcina laborator

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Placinta_Claudiu_Laborator2.Data;
 using Placinta_Claudiu_Laborator2.Models;
+using Placinta_Claudiu_Laborator2.Models.ViewModels;
 
 namespace Placinta_Claudiu_Laborator2.Pages.Categories
 {
@@ -20,13 +21,33 @@ namespace Placinta_Claudiu_Laborator2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                    .ThenInclude(i => i.Book)
+                    .ThenInclude(i => i.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+            
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.BookCategories = category.BookCategories;
             }
+
+            /* if (_context.Category != null)
+             {
+                 Category = await _context.Category.ToListAsync();
+             }
+            */
         }
     }
 }
