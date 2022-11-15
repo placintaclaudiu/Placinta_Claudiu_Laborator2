826b67a084697a8e0d9da532923af4c8f5207270
Placinta Claudiu <claudiuplacinta33@gmail.com>
2022-11-15 16:13:14 +0200

    Sarcina lab 5

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -115,52 +115,122 @@ namespace Placinta_Claudiu_Laborator2.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-           
             returnUrl ??= Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
+            ExternalLogins = (await
+           _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            var user = CreateUser();
+            await _userStore.SetUserNameAsync(user, Input.Email,
+           CancellationToken.None);
+            await _emailStore.SetEmailAsync(user, Input.Email,
+           CancellationToken.None);
+            var result = await _userManager.CreateAsync(user,
+           Input.Password);
+            Member.Email = Input.Email;
+            _context.Member.Add(Member);
+            await _context.SaveChangesAsync();
+            if (result.Succeeded)
             {
-                var user = CreateUser();
+                _logger.LogInformation("User created a new account with password.");
+               
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await
+               _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code =
+               WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+               pageHandler: null,
+               values: new
+               {
+                   area = "Identity",
+                   userId = userId,
+                   code = code,
+                   returnUrl = returnUrl
+               },
+                protocol: Request.Scheme);
+                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+               $"Please confirm your account by <a href = '{HtmlEncoder.Default.Encode(callbackUrl)}' > clicking here </ a >.");
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
 
-                if (result.Succeeded)
+            if(_userManager.Options.SignIn.RequireConfirmedAccount)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    return RedirectToPage("RegisterConfirmation", new
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+                        email = Input.Email,
+                        returnUrl = returnUrl
+                    });
+                }
+                else
+                {
+                    await _signInManager.SignInAsync(user,
+                   isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                } 
+                    ModelState.AddModelError(string.Empty,
+                   error.Description);
+                }
             }
-
-            // If we got this far, something failed, redisplay form
             return Page();
         }
+
+
+        /*
+         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+         {
+
+             returnUrl ??= Url.Content("~/");
+             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+             if (ModelState.IsValid)
+             {
+                 var user = CreateUser();
+
+                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                 var result = await _userManager.CreateAsync(user, Input.Password);
+
+                 Member.Email = Input.Email;
+                 _context.Member.Add(Member);
+                 await _context.SaveChangesAsync();
+
+                 if (result.Succeeded)
+                 {
+                     _logger.LogInformation("User created a new account with password.");
+
+                     var userId = await _userManager.GetUserIdAsync(user);
+                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                     var callbackUrl = Url.Page(
+                         "/Account/ConfirmEmail",
+                         pageHandler: null,
+                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                         protocol: Request.Scheme);
+
+                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                     {
+                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                     }
+                     else
+                     {
+                         await _signInManager.SignInAsync(user, isPersistent: false);
+                         return LocalRedirect(returnUrl);
+                     }
+                 }
+                 foreach (var error in result.Errors)
+                 {
+                     ModelState.AddModelError(string.Empty, error.Description);
+                 } 
+             }
+
+             // If we got this far, something failed, redisplay form
+             return Page();
+         }
+         */
 
         private IdentityUser CreateUser()
         {
---
+++ Migrations/LibraryIdentity/20221115114109_Identity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Placinta_Claudiu_Laborator2.Migrations.LibraryIdentity
+{
+    public partial class Identity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
